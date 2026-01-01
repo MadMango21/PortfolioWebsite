85bba06ab457cd320b2a816b19e44b235d1cc956
Humza Mustafa <107266383+MadMango21@users.noreply.github.com>
2026-01-01 14:01:32 +0000

    added a slider in the skills section

--- js/script.js
+++ js/script.js
@@ -28,24 +28,66 @@ startDelay: 1000,
 showCursor: false
 
 });
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    items: 4,
-    responsive:{
-        0:{
-            items:1
-        },
-        480:{
-            items:2
-        },
-        768:{
-            items:3
-        },
-        938:{
-            items:4
+$(document).ready(function () {
+    var owl = $(".owl-carousel"),
+        // rangeArr = [],
+        inputType = $("input[type=range]");
+    owl.owlCarousel({
+        loop: true,
+        mouseDrag: true,
+        autoplay: false,
+        responsive: {
+            0: {
+                items: 1,
+                slideBy: 1
+
+            },
+            480: {
+                items: 2,
+                slideBy: 1
+
+            },
+            768: {
+                items: 3,
+                slideBy: 1
+            },
+            938: {
+                items: 4,
+                slideBy: 1
+            }
         }
-    }
+
+    });
+    //   function getIndex(event) {
+
+    //   }
+    owl.on('changed.owl.carousel', function (event) {
+        console.log(event.item.index);
+        inputType.val(event.item.index);
+
+    });
+
+    $(".go-me").click(function () {
+        owl.trigger("next.owl.carousel");
+    });
+    $(".back-me").on("click", function () {
+        owl.trigger("prev.owl.carousel");
+    });
+    $("input").on("change", function (e) {
+        e.preventDefault();
+        console.log(inputType.val());
+        // console.log(e.item.index);
+        // FIGURE OUT HOW TO GET CAROUSEL INDEX
+
+        $('.owl-carousel').trigger('to.owl.carousel', [inputType.val(), 1, true]);
+
+    });
 });
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
 
 
 
